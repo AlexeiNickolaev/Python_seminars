6cdeaee4d926e22e67ed202461d9f1d1404d19df
Alexei <datlife2015@yandex.ru>
2023-07-27 11:29:29 +0300

    seminar3

---
+++ seminar3/task3.py
@@ -0,0 +1,13 @@
+# ✔ Создайте вручную кортеж содержащий элементы разных типов.
+# ✔ Получите из него словарь списков, где:
+# ключ — тип элемента,
+# значение — список элементов данного типа.
+
+values = ('ama', 1.002, 12, True, [1, 2, 'hh'], 11, 9.001)
+
+result = {}
+for value in values:
+    result[type(value)] = result.get(type(value), [])
+    result[type(value)].append(value)
+
+print(result)
