8b0ec5e93ed42da4fe2813e854c0483aa42646ac
Alexei <datlife2015@yandex.ru>
2023-08-21 10:49:26 +0300

    seminar10

---
+++ seminar10/task4.py
@@ -0,0 +1,56 @@
+# Создайте класс Сотрудник.
+# Воспользуйтесь классом человека из прошлого задания 3.
+# У сотрудника должен быть:
+# ○ шестизначный идентификационный номер
+# ○ уровень доступа вычисляемый как остаток от деления
+# суммы цифр id на семь
+
+from datetime import datetime
+from random import randint
+
+
+class Person:
+    def __init__(self,
+                 name: str,
+                 surname: str,
+                 year_birth: int) -> None:
+        self.name = name
+        self.surname = surname
+        self.__age = datetime.now().year - year_birth
+
+    def birthday(self):
+        self.__age += 1
+
+    def full_name(self) -> str:
+        return f'{self.name} {self.surname}'
+
+    def age(self):
+        return self.__age
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.surname}'
+
+    def __add__(self, obj):
+        return f'{self.name} += {obj.name}'
+
+
+class Employee(Person):
+    __LOWER_LIM = 100_000
+    __UPPER_LIM = 999_999
+    __LEVEL_DIV = 7
+
+    def __init__(self,
+                 name: str,
+                 surname: str,
+                 year_birth: int) -> None:
+        super().__init__(name, surname, year_birth)
+        self.id = randint(self.__LOWER_LIM,
+                          self.__UPPER_LIM)
+        self.level = sum(map(int, list(str(self.id)))) % self.__LEVEL_DIV
+
+
+e1 = Employee('Alex', 'Nikolaev', 980)
+e2 = Employee('Vasya', 'Pupkin', 981)
+print(e1.id, e1.level)
+print(e1.id, e1.level)
+print(e1 + e2)
